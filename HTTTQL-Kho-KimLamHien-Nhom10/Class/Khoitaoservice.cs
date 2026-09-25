b5970f833d9b5ca41cd2df633437038243d28308
phamhuyen09042004-tech <phamhuyen09042004@gmail.com>
2026-09-25 10:15:15 +0700

    cài thư viện và class dùng chung

--- HTTTQL-Kho-KimLamHien-Nhom10/Class/Khoitaoservice.cs
+++ HTTTQL-Kho-KimLamHien-Nhom10/Class/Khoitaoservice.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HTTTQL_Kho_KimLamHien_Nhom10.Class
 {
     internal class Khoitaoservice
     {
+        public static void KhoiTao()
+        {
+            if (Functions.KiemTraNhanVienTonTai() == true)
+                return;
+
+            TaoAdminMacDinh();
+        }
+        private static void TaoAdminMacDinh()
+        {
+            string matkhauTam = Functions.TaoMatKhauNgauNhien();
+            string hash = BCrypt.Net.BCrypt.HashPassword(matkhauTam, workFactor: 12);
+
+            string sql;
+            sql = "INSERT INTO nhan_vien " +
+                  "(manv, tennv, tendangnhap, matkhau, chucvu, sdt, diachi, email, " +
+                  "lanDNdau, trangthai, ngaycapnhat, ngaytao) " +
+                  "VALUES " +
+                  "(@manv, @tenv, @tendangnhap, @matkhau, @chucvu, @sdt, @diachi, @email, " +
+                  "1, N'Hoạt động', GETDATE(), GETDATE())";
+
+            Functions.Ketnoi();
+
+            SqlCommand cmd = new SqlCommand(sql, Functions.Conn);
+
+            cmd.Parameters.AddWithValue("@manv", "NV001");
+            cmd.Parameters.AddWithValue("@tenv", "Quản trị viên");
+            cmd.Parameters.AddWithValue("@tendangnhap", "admin");
+            cmd.Parameters.AddWithValue("@matkhau", hash);
+            cmd.Parameters.AddWithValue("@chucvu", "Giám đốc");
+            cmd.Parameters.AddWithValue("@sdt", "0123456789");
+            cmd.Parameters.AddWithValue("@diachi", "Hà Tĩnh");
+            cmd.Parameters.AddWithValue("@email", "ceo@kimlamhien.com");
+            cmd.ExecuteNonQuery();
+            Functions.Ngatketnoi();
+
+            MessageBox.Show(
+                "Hệ thống chưa có tài khoản quản trị.\n\n" +
+                "Đã tạo tài khoản admin mặc định:\n\n" +
+                "Tên đăng nhập: admin\n" +
+                "Mật khẩu tạm  : " + matkhauTam + "\n\n" +
+                "Vui lòng đổi mật khẩu sau khi đăng nhập.",
+                "Khởi tạo hệ thống",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
     }
 }
--- HTTTQL-Kho-KimLamHien-Nhom10/Class/session.cs
+++ HTTTQL-Kho-KimLamHien-Nhom10/Class/session.cs
@@ -8,5 +8,60 @@ namespace HTTTQL_Kho_KimLamHien_Nhom10.Class
 {
     internal class session
     {
+        public static string MaNhanVien { get; set; }
+        public static string TenNhanVien { get; set; }
+        public static string TenDangNhap { get; set; }
+        public static string Chucvu { get; set; }
+
+        public static string YeuCauKiemKeNhanVien { get; set; }
+        public static string YeuCauKiemKeKho { get; set; }
+        public static string YeuCauKiemKeChiTiet { get; set; }
+
+        public const string GIAM_DOC = "Giám đốc";
+        public const string QUAN_LY_KHO = "Quản lý kho";
+        public const string NHAN_VIEN_BAN_HANG = "Nhân viên bán hàng";
+        public const string NHAN_VIEN_KE_TOAN = "Nhân viên kế toán";
+        public const string KE_TOAN = "Kế toán";
+        public const string NHAN_VIEN_THU_NGAN = "Nhân viên thu ngân";
+        public static bool LaGiamDoc()
+        {
+            return Chucvu == GIAM_DOC;
+        }
+        public static bool LaQuanLyKho()
+        {
+            return Chucvu == QUAN_LY_KHO;
+        }
+        public static bool LaNhanVienBanHang()
+        {
+            return Chucvu == NHAN_VIEN_BAN_HANG;
+        }
+        public static bool LaNhanVienKeToan()
+        {
+            return Chucvu == NHAN_VIEN_KE_TOAN || Chucvu == KE_TOAN;
+        }
+        public static bool LaNhanVienThuNgan()
+        {
+            return Chucvu == NHAN_VIEN_THU_NGAN;
+        }
+        public static void Dangnhap(string manv, string tenNhanVien, string tenDangNhap, string chucvu)
+        {
+            MaNhanVien = manv;
+            TenNhanVien = tenNhanVien;
+            TenDangNhap = tenDangNhap;
+            Chucvu = chucvu;
+        }
+        public static void Dangxuat()
+        {
+            MaNhanVien = null;
+            TenNhanVien = null;
+            TenDangNhap = null;
+            Chucvu = null;
+        }
+        //nếu đã đăng nhập 
+        public static bool DaDangnhap()
+        {
+            return MaNhanVien != null;
+        }
+        public static bool DaDocYeuCauKiemKe { get; set; } = false;
     }
 }
